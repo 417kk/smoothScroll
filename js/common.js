e0775c0febd48872e7cf9544a767df27fd1bcc99
Kazuho Kanazawa 金澤一穂 <k_kanazawa@stagegroup.jp>
2017-10-12 17:47:13 +0900

    headerの値を実数に変更

--- js/common.js
+++ js/common.js
@@ -154,28 +154,30 @@ $(function(){
 ---------------------------------------------------------------------------------------------------- */
 /* anchor */
 $(window).on('load resize', function() {
-	var headerHeight = $('#l-header').outerHeight();
 	var url = $(location).attr('href');
 
 	/*external links*/
 	if(url.indexOf('/#') != -1){
 		var id = url.split('/#');
 		var $target = $('#' + id[id.length - 1]);
+		var headerHeight = ($target.offset().top > 111) ? 51 : 111;
 		if($target.length){
-			var pos = $target.offset().top-headerHeight;
+			var pos = $target.offset().top - headerHeight;
 			$("html, body").animate({scrollTop:pos}, 1);
 		}
 	}
 
 	/*internal links*/
 	$('a[href*="#"]').on('click', function() {
-		var headerHeight = $('#l-header').outerHeight();
 		var href = $(this).attr('href');
 		var id = href.split('#');
 		var $target = $('#' + id[id.length - 1]);
+		var headerHeight = ($target.offset().top > 111) ? 51 : 111;
 		if($target.length){
 			var pos = $target.offset().top - headerHeight;
-			$("html, body").animate({scrollTop:pos}, 600, 'easeInOutQuart');
+			$("html, body").animate({
+				scrollTop:pos
+			}, 600, 'easeInOutQuart');
 			return false;
 		}
 	});
