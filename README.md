debe1a45fa32b25f5a87a996ba3b4ff2135f1c74
Kazuho Kanazawa 金澤一穂 <k_kanazawa@stagegroup.jp>
2018-01-23 17:09:59 +0900

    ウィンドウリサイズ時に対応

--- README.md
+++ README.md
@@ -14,10 +14,11 @@ cにブレイクポイントの値を入れてください。
 2.上記とかぶりますが、スクロールしてヘッダーの高さが変わるような仕様にも対応してません。  
 
 	$(window).on('load', function() {
-		var a, b, c;
+		var a, b, c, d;
 		a = 48; //sp height
 		b = 111; // pc height
 		c = 992; // break point
+		d = $('#l-header'); // header element
 
 		var url = $(location).attr('href');
 		var window_width = $(window).width();
@@ -35,12 +36,12 @@ cにブレイクポイントの値を入れてください。
 
 		/*internal links*/
 		$('a[href*="#"]').on('click', function() {
-			window_width = $(window).width();
 			var href = $(this).attr('href');
 			var id = href.split('#');
 			var $target = $('#' + id[id.length - 1]);
+			var marginTop = d.outerHeight();
 			if($target.length){
-				var pos = $target.offset().top - h;
+				var pos = $target.offset().top - marginTop;
 				$("html, body").animate({
 					scrollTop:pos
 				}, 600, 'easeInOutQuart');
--- js/common.js
+++ js/common.js
@@ -154,13 +154,15 @@ $(function(){
 ---------------------------------------------------------------------------------------------------- */
 /* anchor */
 $(window).on('load', function() {
-	var a, b;
+	var a, b, c, d;
 	a = 48; //sp height
 	b = 111; // pc height
+	c = 992; // break point
+	d = $('#l-header'); // header element
 
 	var url = $(location).attr('href');
 	var window_width = $(window).width();
-	var h = (window_width < 992) ? a : b;
+	var h = (window_width < c) ? a : b;
 
 	/*external links*/
 	if(url.indexOf('#') != -1){
@@ -174,12 +176,12 @@ $(window).on('load', function() {
 
 	/*internal links*/
 	$('a[href*="#"]').on('click', function() {
-		window_width = $(window).width();
 		var href = $(this).attr('href');
 		var id = href.split('#');
 		var $target = $('#' + id[id.length - 1]);
+		var marginTop = d.outerHeight();
 		if($target.length){
-			var pos = $target.offset().top - h;
+			var pos = $target.offset().top - marginTop;
 			$("html, body").animate({
 				scrollTop:pos
 			}, 600, 'easeInOutQuart');
